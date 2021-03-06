60f1d783b4517577da82cb9eb77e27a69468f41a
Cris Javier <crisj@hawaii.edu>
2021-03-05 23:20:22 -1000

    Section 1B

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
+import { Task } from './Task';
+
+const tasks = [
+  {_id: 1, text: 'Get Insurance Number from Medquest'},
+  {_id: 2, text: 'Finish Experiences for ICS 314'},
+  {_id: 3, text: 'Start on'},
+];
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
-    <Hello/>
-    <Info/>
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+    </ul>
   </div>
 );
+
+
+
+
