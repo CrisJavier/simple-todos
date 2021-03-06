830fe1a037611ab4b408f3f4796e1c992166798d
Cris Javier <crisj@hawaii.edu>
2021-03-06 11:19:33 -1000

    Section 3

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -4,24 +4,26 @@ import { Info } from './Info.jsx';
 import { Task } from './Task';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
+import { TaskForm } from './TaskForm';
 
 const tasks = [
-  {_id: 1, text: 'Get Insurance Number from Medquest'},
-  {_id: 2, text: 'Finish Experiences for ICS 314'},
-  {_id: 3, text: 'Start on'},
+  { _id: 1, text: 'Get Insurance Number from Medquest' },
+  { _id: 2, text: 'Finish Experiences for ICS 314' },
+  { _id: 3, text: 'Start on' },
 ];
 
 export const App = () => {
-  
-const tasks = useTracker(() => TasksCollection.find({}).fetch());
-return(
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <ul>
-      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
-);
+
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  return (
+      <div>
+        <h1>Welcome to Meteor!</h1>
+        <TaskForm/>
+        <ul>
+          {tasks.map(task => <Task key={task._id} task={task}/>)}
+        </ul>
+      </div>
+  );
 };
 
 
